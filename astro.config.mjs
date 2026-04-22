361ea6721146a9fc2aea9835342553c7cde76df3
A. Todd Emerson <emerson@pluralfusion.com>
2026-04-22 14:48:34 -0400

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
+  output: 'static',
   site: 'https://terndemo.hottoddie.org',
   vite: {
     plugins: [tailwindcss()]
